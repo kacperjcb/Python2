b7a5983016151b1a6e1aa81c64d52084129afe0b
kacperjcb <kacperjcb@gmail.com>
2023-11-09 14:53:31 +0100

    Change test's to pytest

--- test.py
+++ test.py
@@ -1,48 +1,59 @@
-import unittest
+import pytest
 from PyQt5.QtWidgets import QApplication, QTextEdit
 from main import MyTestApp
 
 
-class Test(unittest.TestCase):
-    def test_ipv4_info(self):
-        app = QApplication([])
-        window = MyTestApp(app)
-        window.get_ipv4_info()
-        text_output = window.text_output.toPlainText()
-        self.assertIn("IP:", text_output)
-        self.assertIn("Static:", text_output)
-        self.assertIn("Interface:", text_output)
-        app.quit()
-
-    def test_system_info(self):
-        app = QApplication([])
-        window = MyTestApp(app)
-        window.get_system_info()
-        text_output = window.text_output.toPlainText()
-        self.assertIn("OS Version:", text_output)
-        self.assertIn("Architecture:", text_output)
-        self.assertIn("Cores:", text_output)
-        self.assertIn("RAM:", text_output)
-        app.quit()
-
-    def test_bios_info(self):
-        app = QApplication([])
-        window = MyTestApp(app)
-        window.get_bios_info()
-        text_output = window.text_output.toPlainText()
-        self.assertIn("BIOS Vendor:", text_output)
-        self.assertIn("BIOS Version:", text_output)
-        self.assertIn("BIOS Release Date:", text_output)
-        app.quit()
-
-    def test_hostname(self):
-        app = QApplication([])
-        window = MyTestApp(app)
-        window.get_hostname()
-        text_output = window.text_output.toPlainText()
-        self.assertIn("Host Name:", text_output)
-        app.quit()
+@pytest.fixture
+def app():
+    return QApplication([])
 
 
-if __name__ == '__main__':
-    unittest.main()
+@pytest.fixture
+def window(app):
+    return MyTestApp(app)
+
+
+def test_ipv4_info(window):
+    window.get_ipv4_info()
+    text_output = window.text_output.toPlainText()
+    assert "IP:" in text_output
+    assert "Static:" in text_output
+    assert "Interface:" in text_output
+
+
+def test_system_info(window):
+    window.get_system_info()
+    text_output = window.text_output.toPlainText()
+    assert "OS Version:" in text_output
+    assert "Architecture:" in text_output
+    assert "Cores:" in text_output
+    assert "RAM:" in text_output
+
+
+def test_bios_info(window):
+    window.get_bios_info()
+    text_output = window.text_output.toPlainText()
+    assert "BIOS Vendor:" in text_output
+    assert "BIOS Version:" in text_output
+    assert "BIOS Release Date:" in text_output
+
+
+def test_hostname(window):
+    window.get_hostname()
+    text_output = window.text_output.toPlainText()
+    assert "Host Name:" in text_output
+
+
+def test_display_help(window):
+    help_text = window.display_help()
+
+    expected_help_text = (
+        "Available commands:\n"
+        "get_ipv4_info - Get IPv4 information\n"
+        "get_proxy_info - Get proxy information\n"
+        "get_system_info - Get system information\n"
+        "get_bios_info - Get BIOS information\n"
+        "get_hostname - Get host name"
+    )
+
+    assert help_text == expected_help_text
